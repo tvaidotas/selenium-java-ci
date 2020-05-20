bc7343df799ab328e7c2fb998735f071c9ff668e
tvaidotas <tadas.vaidotas@gmail.com>
2020-05-20 14:14:42 +0100

    added an example test added a before method to close the browser

--- src/test/java/com/qa/JavaSeleniumTest.java
+++ src/test/java/com/qa/JavaSeleniumTest.java
@@ -1,9 +1,15 @@
 package com.qa;
 
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.Assert.assertTrue;
 
 public class JavaSeleniumTest {
 
@@ -27,6 +33,28 @@ public class JavaSeleniumTest {
 
     public boolean isWindows(){
         return System.getProperty("os.name").toLowerCase().contains("win");
+    }
+
+    @Test
+    public void exampleSelenium() throws InterruptedException {
+        Thread.sleep(1000);
+        driver.get("http://www.google.com/");
+        Thread.sleep(1000);
+        WebElement googleSearchField = driver.findElement(By.name("q"));
+        googleSearchField.sendKeys("funny cat pics");
+        Thread.sleep(2000);
+        googleSearchField.submit();
+        Thread.sleep(2000);
+        WebElement pictures = driver.findElement(By.partialLinkText("Images for funny cat"));
+        pictures.click();
+        Thread.sleep(10000);
+        WebElement selectedPicture = driver.findElement(By.className("NZmxZe"));
+        assertTrue(selectedPicture.isDisplayed());
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
     }
 
 }
